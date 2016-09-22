f5bdd0c709e40c330c46571bdc24f6d1a35d69e4
Tlareg Aivir <tlaregaivir@gmail.com>
2016-09-22 23:06:13 +0200

    haskell learning

--- src/examples.hs
+++ src/examples.hs
@@ -1,15 +1,26 @@
 -- install Haskell Platform https://www.haskell.org/platform/
 
--- https://wiki.haskell.org/Tutorials
-  -- https://learnxinyminutes.com/docs/haskell/
+  -- https://wiki.haskell.org/Tutorials
+  -- http://stackoverflow.com/questions/1012573/getting-started-with-haskell
+
   -- http://learnyouahaskell.com/chapters
+  -- http://book.realworldhaskell.org/read/
+
+  -- https://learnxinyminutes.com/docs/haskell/
+  -- https://wiki.haskell.org/99_questions/1_to_10
+
 
 -- run GHC interactive from command line: ghci
 
 -- load functions from examples.hs by :l examples
 -- reload file by :r
+-- load modules with :m + Data.list Data.Map Data.Set
 -- check type :t valueToCheck
 
+
+
+
+import Data.List
 
 -- function
 main = do
@@ -34,6 +45,11 @@ nestedFnExample = do
   let myMul a b = a * b
   print (myMul 2 5)
 
+
+
+--------------------------------------
+-- LISTS
+--------------------------------------
 
 listExample = do
 
@@ -78,20 +94,11 @@ listExample = do
   -- [5, 5, 5]
   print (replicate 3 5)
 
-  -- list comprehension
-  -- make list of doubled numbers from 1 to 10
-  print ([x*2 | x <- [1..10]] )
-
-  -- with condition (or predicate, we can use many of them after comma)
-  print ([x*2 | x <- [1..10], x*2 >= 12] )
-
-  -- function that takes list xs, 
-  -- filter odd numbers and map it to boom if greater than 10 or to bang
-  let boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]  
-  print (boomBangs [8, 9, 11, 12])
 
 
-
+--------------------------------------
+-- TUPLES
+--------------------------------------
 
 tuplesExample = do
 
@@ -104,11 +111,35 @@ tuplesExample = do
   -- zip takes two lists and creates one list of tuples
   print (zip [1 .. 5] ["one", "two", "three", "four", "five"])
 
+
+
+--------------------------------------
+-- LIST COMPREHENSION
+--------------------------------------
+
+listCompExample = do
+  
+  -- make list of doubled numbers from 1 to 10
+  print ([x*2 | x <- [1..10]] )
+
+  -- with condition (or predicate, we can use many of them after comma)
+  print ([x*2 | x <- [1..10], x*2 >= 12] )
+
+  -- function that takes list xs, 
+  -- filter odd numbers and map it to boom if greater than 10 or to bang
+  let boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]  
+  print (boomBangs [8, 9, 11, 12])
+
   -- which right triangle that has integers for all sides
   -- and all sides equal to or smaller than 10 has a perimeter of 24
   let rightTriangles = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a+b+c == 24]
   print rightTriangles
 
+
+
+--------------------------------------
+-- FUNCTIONS
+--------------------------------------
 
 -- function with type declaration
 removeNonUppercase :: [Char] -> [Char]  
@@ -118,7 +149,7 @@ removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
 addThree :: Int -> Int -> Int -> Int  
 addThree x y z = x + y + z  
 
--- pattern matching
+-- guard patterns
 sayMe :: (Integral a) => a -> String  
 sayMe 1 = "One!"  
 sayMe 2 = "Two!"  
@@ -148,6 +179,12 @@ zip' _ [] = []
 zip' [] _ = []
 zip' (a:aa) (b:bb) = (b, a): zip' aa bb
 
+
+
+--------------------------------------
+-- HIGH ORDER FUNCTIONS
+--------------------------------------
+
 map' :: (a -> b) -> [a] -> [b]  
 map' _ [] = []  
 map' f (x:xs) = f x : map f xs
@@ -160,3 +197,21 @@ filter' predicateFn (x:xs)
 
 filter'' :: (a -> Bool) -> [a] -> [a]  
 filter'' p = foldr (\x acc -> if p x then x : acc else acc) []  
+
+-- fuctions composition with "."
+dictGetValByKey dict key = snd . head $ filter (\(k, v) -> k == key) dict
+
+
+
+--------------------------------------
+-- CUSTOM TYPES
+--------------------------------------
+
+data Person = Person { firstName :: String, lastName :: String } deriving (Show)
+
+introducePerson :: Person -> IO()
+introducePerson p = putStrLn $ "Hi, I'am " ++ firstName p ++ " " ++ lastName p
+
+introduceExample = do
+  let dude = Person "Jan" "Kowalski"
+  introducePerson dude
